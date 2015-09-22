9e8057e980bb00d42700bef2ea0f290329c553b8
RMCornell <zen.alchemist5280@gmail.com>
2015-09-22 10:53:52 -0600

    modal working with basic article information

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'nytimes_newswire#index'
 
-  get '/newswire', to: "nytimes_newswire#index"
-  get '/newswire/:url', to: 'nytimes_newswire#show'
+  get '/newswires', as: "newswires", to: "nytimes_newswire#index"
+  get '/newswires/:id', as: "newswire", to: 'nytimes_newswire#show'
 
 end
