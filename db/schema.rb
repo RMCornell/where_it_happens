dc7b4e188ced9f43d1be96c20b76344c41a9bcd9
RMCornell <zen.alchemist5280@gmail.com>
2015-09-22 23:33:23 -0600

    twitter authentication working and index page styling updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922210551) do
+ActiveRecord::Schema.define(version: 20150923052241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                  default: ""
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
